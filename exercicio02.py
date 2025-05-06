805415e5e687972bf1c278fef71d0d270938d62e
sant0hh <00001116121335sp@al.educacao.sp.gov.br>
2025-05-06 12:12:04 -0300

    criado novas funcionalidades para funcao

---
+++ exercicio02.py
@@ -0,0 +1,10 @@
+def somar_num(lista):
+    total = 0
+    for i in lista:
+        total += i
+    return total
+
+numeros = [1,2,3,4,5,6]
+
+print(somar_num(numeros))
+
---
+++ exercicio05.py
@@ -0,0 +1,11 @@
+def cadastrar_aluno(nome, email, serie, nota01, nota02, nota03):
+    alunos = []
+    aluno = {
+        "nome": nome,
+        "email": email,
+        "serie": serie
+        "notas": [nota01, nota02, nota03] 
+    }
+    alunos.append
+    return alunos
+print(cadastrar_alunos("davi", "davisak08@gmail.com", "2tb", 10, 9, 8))
