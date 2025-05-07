d2180360c016937767442018eb61e6f6f064a66f
sant0hh <00001116121335sp@al.educacao.sp.gov.br>
2025-05-07 08:02:21 -0300

    cadastrodefilmes

---
+++ exercicio06.py
@@ -0,0 +1,49 @@
+def cadastrar_filmes(nome, classificacao, descricao, categoria01, categoria02, categoria03):
+    filmes = []
+    filme = {
+        "nome": nome,
+        "classificacao": classificacao,
+        "descricao": descricao,
+        "categoria": [categoria01, categoria02, categoria03]
+    }
+    filmes.append
+    return filmes
+print(cadastrar_filmes("netflix", 16, "filmes e series", "terror", "comedia", "acao"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
